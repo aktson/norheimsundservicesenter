ad57393109d0ff0fef826340cae5e7326d29f8f5
Ankit Soni <akt_soni@yahoo.co.in>
2022-02-14 22:25:34 +0100

    heim 2 removed

--- js/index.js
+++ js/index.js
@@ -2,26 +2,34 @@ const carouselInner = document.querySelector("#carousel-inner");
 const postsEmbedUrl = "http://localhost/norhss/wp-json/wp/v2/posts/?_embed";
 
 async function fetchPosts() {
-    const response = await fetch(postsEmbedUrl);
-    const data = await response.json();
-    data.forEach(item => {
-        createHtml(item)
-        console.log(item)
-    });
+    try {
+        const response = await fetch(postsEmbedUrl);
+        const data = await response.json();
+        data.forEach((item) => {
+            createHtml(item)
+            console.log(item)
+            console.log(item._embedded["wp:featuredmedia"][0].source_url)
+        });
+    }
+    catch (error) {
+
+        console.log(error)
+    }
+
 }
 fetchPosts()
 
 function createHtml(result) {
 
-    const image = result._embedded["wp:featuredmedia"][0].source_url;
+    const receivedImage = result._embedded["wp:featuredmedia"][0].source_url;
 
     carouselInner.innerHTML += ` 
     <div class="carousel-item " data-bs-interval="10000" >
-        <div class="ratio ratio-21x9" style="height:60vh;background:url('${image}')  center / cover no-repeat;background-color: rgba(0,0,0,0.5); background-blend-mode: overlay;">
+        <div class="ratio ratio-21x9" style="height:60vh;background:url('${receivedImage}')  center / cover no-repeat;background-color: rgba(0,0,0,0.5); background-blend-mode: overlay;">
             </div>
-        <div class="carousel-caption d-none d-md-block p-2 " style=" margin: 0 auto;" id="caption">
-        <h3 class="fs-1 mb-3">${result.title.rendered} </h3>
-        <p class="fs-4">${result.content.rendered}</p>
+        <div class="carousel-caption  d-none d-md-block p-2 "  id="caption" >
+        <h5 class="fs-3 mb-3">${result.title.rendered} </h5>
+        <p >${result.content.rendered}</p>
         </div>
     </div>
     `
--- js/produkter.js
+++ js/produkter.js
@@ -6,27 +6,35 @@ const accordionContainer = document.querySelector(".accordion-container");
 
 
 const fetchProducts = async () => {
+    try {
+        const response = await fetch(productUrl);
+        const data = await response.json();
 
-    const response = await fetch(productUrl);
-    const data = await response.json();
-
-    data.forEach(product => {
-        console.log(product.tags[0].name)
-        createHtmlBtn(product)
-        createHtmlBody(product)
-        createAccordion(product)
-    });
+        data.forEach(product => {
+            createHtmlBtn(product)
+            createHtmlBody(product)
+            createAccordion(product)
+        });
+    }
+    catch (error) {
+        console.log(error)
+    }
 }
 fetchProducts();
+
+
 function createHtmlBtn(result) {
 
+    const receivedTag = result.tags[0].name.trim().toLowerCase();
     tabsBtnContainer.innerHTML += `
-    <button class="nav-link p-4" id="v-pills-${result.tags[0].name.toLowerCase()}-tab" data-bs-toggle="pill" data-bs-target="#v-pills-${result.tags[0].name.toLowerCase()}" type="button" role="tab" aria-controls="v-pills-${result.tags[0].name}.toLowerCase()}" aria-selected="false">${result.name}</button>
+    <button class="nav-link p-4" id="v-pills-${receivedTag}-tab" data-bs-toggle="pill" data-bs-target="#v-pills-${receivedTag}" type="button" role="tab" aria-controls="v-pills-${receivedTag}" aria-selected="false">${result.name}</button>
 `
 }
+
 function createHtmlBody(result) {
+    const receivedTag = result.tags[0].name.trim().toLowerCase();
     tabsBodyContainer.innerHTML += `
-        <div class="tab-pane fade show p-2 " id="v-pills-${result.tags[0].name.toLowerCase()}" role="tabpanel" aria-labelledby="v-pills-${result.tags[0].name.toLowerCase()}-tab">
+        <div class="tab-pane fade show p-2 " id="v-pills-${receivedTag}" role="tabpanel" aria-labelledby="v-pills-${receivedTag}-tab">
             <h2>${result.name}</h2>
             <p >${result.description}</p>
             <figure class"mx-auto"><img src="${result.images[0].src}" alt="${result.images[0].alt}" class="img-fluid p-4 ">
@@ -35,14 +43,17 @@ function createHtmlBody(result) {
 }
 
 function createAccordion(result) {
+
+    const receivedTag = result.tags[0].name.trim().toLowerCase();
+
     accordionContainer.innerHTML += `
         <div class="accordion-item">
             <h3 class="accordion-header" id="headingTwo">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#${result.tags[0].name.toLowerCase()}" aria-expanded="false" aria-controls="${result.tags[0].name.toLowerCase()}">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#${receivedTag}" aria-expanded="false" aria-controls="${receivedTag}">
             ${result.name}
             </button>
             </h3>
-            <div id="${result.tags[0].name.toLowerCase()}" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+            <div id="${receivedTag}" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
             <div class="accordion-body">
             <p>${result.description}</p>
             <img src="${result.images[0].src}" alt="${result.images[0].alt}" class="w-50 img-fluid">
@@ -51,6 +62,5 @@ function createAccordion(result) {
             </div>
         </div>
   `
-
 }
 
