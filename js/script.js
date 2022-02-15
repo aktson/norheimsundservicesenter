1cf5e67ac82d1333ab33ab2a94d55c4941c2cb03
Ankit Soni <akt_soni@yahoo.co.in>
2022-02-15 10:12:11 +0100

    content added to homepage

--- js/script.js
+++ js/script.js
@@ -10,16 +10,16 @@ toTopBtn.onclick = function () {
 }
 
 // page on load loader function
-const spinner = document.querySelector("#spinner");
-const body = document.querySelector("body");
+// const spinner = document.querySelector("#spinner");
+// const body = document.querySelector("body");
 
-document.onreadystatechange = function () {
-    if (document.readyState !== "complete") {
-        spinner.style.visibility = "visible";
-        body.style.display = "none";
-    } else {
-        spinner.style.display = "none";
-        body.style.display = "block"
-    }
-}
+// document.onreadystatechange = function () {
+//     if (document.readyState !== "complete") {
+//         spinner.style.visibility = "visible";
+//         body.style.display = "none";
+//     } else {
+//         spinner.style.display = "none";
+//         body.style.display = "block"
+//     }
+// }
 
