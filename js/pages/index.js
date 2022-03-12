4ffdff0b2728cb3919ff9dcbb6829e138f900a90
Ankit Soni <akt_soni@yahoo.co.in>
2022-03-12 22:07:09 +0100

    about and carousel fetch request added

--- js/pages/index.js
+++ js/pages/index.js
@@ -7,18 +7,59 @@ import { takeToTop } from "../script.js";
 createMenu();
 takeToTop();
 
+
+//about section fetch
 const carouselInner = document.querySelector("#carousel-inner");
 
-const url = baseUrl + "wp/v2/posts?_embed";
-(async function fetchPosts() {
 
+(async function fetchPage() {
+    const url = baseUrl + "api/pages/1";
 
     try {
         const response = await fetch(url);
 
         if (response.ok) {
-            const data = await response.json();
-            createHtml(data)
+            const results = await response.json();
+            const data = results.data.attributes
+            renderAboutSection(data)
+        }
+
+
+    }
+    catch (error) {
+        console.log(error)
+    }
+
+})();
+
+
+function renderAboutSection(result) {
+    const aboutContainer = document.querySelector("#about-container");
+
+    // const result = results.attributes;
+    console.log(result)
+    aboutContainer.innerHTML += `  
+        <div class="lc-block">
+                <h2 >${result.title}</h2>
+        </div>
+        <div class="lc-block col-lg-6 mx-auto mb-4 line-break">
+        <p class="lead line-break">${result.description}</p>
+                
+        </div>`
+}
+
+
+
+
+(async function fetchPosts() {
+    const url = baseUrl + "api/posts?populate=*";
+
+    try {
+        const response = await fetch(url);
+
+        if (response.ok) {
+            const results = await response.json();
+            createHtml(results.data)
         }
 
     }
@@ -26,20 +67,22 @@ const url = baseUrl + "wp/v2/posts?_embed";
         console.log(error)
     }
 
-})()
+})();
 
 
 function createHtml(results) {
+    const carouselInner = document.querySelector("#carousel-inner");
 
     results.forEach((result) => {
-        const receivedImage = result._embedded["wp:featuredmedia"][0].source_url;
+        const post = result.attributes;
+        const receivedImage = post.img.data.attributes.url
 
         carouselInner.innerHTML += ` <div class="carousel-item " data-bs-interval="10000" >
                                     <div class="ratio ratio-21x9" style="height:60vh;background:url('${receivedImage}')  center / cover no-repeat;background-color: rgba(0,0,0,0.5); background-blend-mode: overlay;">
                                         </div>
                                     <div class="carousel-caption  d-none d-md-block p-2 "  id="caption" >
-                                    <h5 class="fs-3 mb-3">${result.title.rendered} </h5>
-                                    <p >${result.content.rendered}</p>
+                                    <h5 class="fs-3 mb-3">${post.title} </h5>
+                                    <p >${post.description}</p>
                                     </div>
                                 </div>
     `
@@ -58,3 +101,5 @@ data.forEach(item => {
                                         <p >${item.description}</p>
                                     </div>`
 })
+
+
